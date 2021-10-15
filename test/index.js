76d56531e86625cf695f5635a0ede40eb7561d76
adeola daniel <50976837+adeoladdaniel@users.noreply.github.com>
2021-10-15 11:08:17 -0700

    Add files via upload

---
+++ test/index.js
@@ -0,0 +1,25 @@
+
+
+ var ETH	= "3,478.25"
+var BNB = "433.06"
+var ADA ="2.1999"
+var USDT="1.0004"
+var USD = array([
+    'ETH','BNB','USDT'
+]
+)
+
+
+
+
+
+if(USD=='ETH'){
+document.write("")   
+}
+
+
+
+
+
+
+                 
